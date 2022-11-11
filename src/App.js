1e7d03c195514c53f372adfcc5ee11ee6777b7fa
Daniel Stryhn <dsl@one.com>
2022-11-11 16:35:05 +0100

    Prepare repository for the test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>One.com Code Challenge</h1>
       </header>
+      <p>
+        Extend this application with functionality that can calculate the difference in days between to dates given by the user.
+        The result should be something that you would think of as production ready. 
+      </p>
+      <p>
+        <b>Example:</b>
+      </p>
+      <p>
+        Start date: 2021-01-01
+        <br />
+        End date: 2022-01-01
+        <br />
+        Result: 365
+      </p>
+      <p><b>Rules:</b></p>
+      <ul>
+        <li>It is not allowed to use built-in date functionality</li>
+        <li>The start day <strong>should</strong> be included in the result</li>
+        <li>The end day <strong>should not</strong> be included in the result</li>
+        <li>Styling is optional</li>
+        <li>Be creative with the solution</li>
+      </ul>
     </div>
   );
 }
